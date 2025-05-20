fd2403f273691c82e130d3904b57a50d2aaf74cd
joaorabelo7 <joao.rabelo2@etec.sp.gov.br>
2025-05-19 23:44:05 -0300

    aula2

---
+++ Numbers/index.js
@@ -0,0 +1,15 @@
+let num1 = 0.7;
+let num2 = 0.1;
+
+
+
+
+
+
+// console.log(num1.toString() + num2);
+// console.log(typeof num1)
+// num1 = num1.toString(); converte para str 
+// console.log(num1.toFixed(2));  // EScolhe qnts casas decimais quer 
+// console.log(Number.isInteger(num1)); // Verifica se o numero eh inteiro. Retorna False ou True
+// let temp = num1 * 'Ola';
+// console.log(Number.isNaN(temp));  // Verifica se a variavel contem um NaN (Not a Number)
