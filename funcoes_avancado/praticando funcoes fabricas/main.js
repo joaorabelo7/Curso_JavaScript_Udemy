cd1e66d2b8a26ce4e2bbbf5e73d71d52614b8bb4
joaorabelo7 <joao.rabelo2@etec.sp.gov.br>
2025-07-16 19:17:59 -0300

    calculadora com factory faction

--- funcoes_avancado/praticando funcoes fabricas/main.js
+++ funcoes_avancado/praticando funcoes fabricas/main.js
@@ -1,30 +1,73 @@
 function criaCalculadora() {
     return {
         display: document.querySelector('.display'),
+        btnClear: document.querySelector('.btn-clear'),
+
         
-
-
-
-
-
-
         inicia(){
             this.cliqueBotoes();
+            this.clearDisplay();
+            this.pressionaEnter();
+            
         },
-
+        
         cliqueBotoes(){
-            document.addEventListener("click", function(e){
-                const el = e.target;
-
+            //this -> calculdaora
+            document.addEventListener("click", (e) => {
+                const el = e.target; 
+                
                 if(el.classList.contains("btn-num")){
-                    this.btnParaDisplay();
+                    this.btnParaDisplay(el.innerText);
+                }
+                if(el.classList.contains("btn-clear")){
+                    this.clearDIsplay();
+                }
+                if(el.classList.contains("btn-del")){
+                    this.apagaUm();
+                }
+                if(el.classList.contains("btn-eq")){
+                    this.realizaConta();
                 }
             });
         },
+        
+        btnParaDisplay(valor){
+            this.display.value += valor;
+        },
 
-        btnParaDisplay(){
-            
+        clearDisplay(){
+            this.display.value = '';
+        },
+
+        apagaUm(){
+            this.display.value = this.display.value.slice(0,-1)
+        },
+
+        realizaConta(){
+            let conta = this.display.value;
+
+            try{
+                conta = eval(conta);
+
+                if(!conta){
+                    alert("Conta invalida");
+                    return;
+                }
+
+                this.display.value = String(conta)
+            }catch(e){
+                alert("Conta invalida");
+            }
+        },
+
+        pressionaEnter(){
+            this.display.addEventListener('keyup',e => {
+                if(e.key === 'Enter'){
+                    this.realizaConta();
+                }
+            })
         }
+
     };
 }
 
