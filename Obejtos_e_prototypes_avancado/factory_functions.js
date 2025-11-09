8beec9b7d1472351a1da48249c688f0e0502c5be
joaorabelo7 <joao.rabelo2@etec.sp.gov.br>
2025-11-09 19:00:35 -0300

    Programção orientada a objetos

---
+++ Obejtos_e_prototypes_avancado/factory_functions.js
@@ -0,0 +1,14 @@
+function CriaPessoa(nome,sobrenome){
+    const pessoaPrototype ={
+        
+    }
+    return{
+        nome,
+        sobrenome,
+
+    };
+};
+
+const p1 = CriaPessoa('joao','rabelo');
+
+console.log(p1)
