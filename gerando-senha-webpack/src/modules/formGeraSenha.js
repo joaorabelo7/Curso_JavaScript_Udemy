9b3d3e612698d8ac65d71f64da5b5ed20ece97d0
joaorabelo7 <joao.rabelo2@etec.sp.gov.br>
2025-12-23 14:01:00 -0300

    gerador de senha com webpack

---
+++ gerando-senha-webpack/src/modules/formGeraSenha.js
@@ -0,0 +1,26 @@
+import geraSenha from "./geradores";
+
+const senhaGerada = document.querySelector('.senha-gerada');
+const qtdCaracteres = document.querySelector('.qtd-caracteres');
+const chkMaisculas = document.querySelector('.chk-maiusculas');
+const chkMinusculas = document.querySelector('.chk-minusculas');
+const chkNumeros = document.querySelector('.chk-numeros');
+const chkSimbolos = document.querySelector('.chk-simbolos');
+const gerarSenha = document.querySelector('.gerar-senha')
+
+export default () => {
+    gerarSenha.addEventListener('click', () => {
+        senhaGerada.innerHTML = gera()
+    })
+}
+
+function gera() {
+    const senha = geraSenha(
+        qtdCaracteres.value,
+        chkMaisculas.checked,
+        chkMinusculas.checked,
+        chkNumeros.checked,
+        chkSimbolos.checked
+    );
+    return senha || 'Nada selecionado';
+}
