e674fdd43ca7dccbd937e93a58e109fe0775a73c
joaorabelo7 <joao.rabelo2@etec.sp.gov.br>
2026-01-06 22:09:12 -0300

    session no express

---
+++ node/ExpressSession/server.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+
+mongoose.connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    app.emit('pronto');
+  })
+  .catch(e => console.log(e));
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
+const routes = require('./routes');
+
+// Body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Arquivos estáticos
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+// Session
+const sessionOptions = session({
+  secret: 'adasdasdasdasdasdas',
+  store: MongoStore.create({
+    mongoUrl: process.env.CONNECTIONSTRING,
+  }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 dias
+    httpOnly: true,
+  },
+});
+
+app.use(sessionOptions);
+app.use(flash());
+
+// Views
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+// Rotas
+app.use(routes);
+
+// Servidor
+app.on('pronto', () => {
+  app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+  });
+});
